72b78bba3653ecc88c03f3bd656ba19221e0d5f4
Joel Batista Maciel da Paz <joel.paz@idealtrends.local>
2025-12-02 22:51:29 -0300

    finalizado mais um kata

--- your-order-please/solucao.js
+++ your-order-please/solucao.js
@@ -1,27 +1,44 @@
 function order(words) {
-    // ...
 
+    // separar as palavras em Array
     let palavraSeparada = words.split(' ')
-    // console.log(palavraSeparada)
+
+    // um array para armazenar na ordem certa
+    let fraseParaOrganizar = []
+    // uma variavel para deixar como uma frase e não como array
     let fraseOrganizada = ""
 
+    // um loop para criar os indices do array
+    for (palavra of palavraSeparada) {
+        fraseParaOrganizar.push(palavra)
+    }
+    
+    // um loop que passa em todos os elementos
     palavraSeparada.forEach((elemento) => {
-
+        
+        // um loop que busca o numero na palavra
         for (let i = 1; i <= palavraSeparada.length; i++) {
-
-            let indice = i.toString()
             
-            let verificacao = elemento.includes(indice)
-
-            // console.log(verificacao)
-
+            // colocar True ou False dependendo se achar ou não
+            let verificacao = elemento.includes(i)
+            
+            // se for True adiciona no array
             if (verificacao) {
-                fraseOrganizada += `${palavraSeparada[i - 1]} `
+                
+                // adiciona no array no indice correto
+                fraseParaOrganizar[i - 1] = `${elemento}`
+                
             }
         }
     });
+
+    // um loop para tirar do array e transformar em string
+    for (const palavra of fraseParaOrganizar) {
+        fraseOrganizada += `${palavra} `
+    }
+
     console.log(fraseOrganizada)
-    return fraseOrganizada
+    return fraseOrganizada.trim()
 }
 
 order("is2 Thi1s T4est 3a")
